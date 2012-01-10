1e4755e0ed9decd5a704d3dac59294b65b55678d
Chuck Greb <charles.greb@gmail.com>
2012-01-10 00:05:53 -0500

    Adds ShadowDateFormat for simple test cases

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowDateFormat.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowDateFormat.java
@@ -6,11 +6,57 @@ import android.text.format.DateFormat;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
+/**
+ * Shadow of {@link DateFormat} class.
+ *
+ * @author Chuck Greb <charles.greb@gmail.com>
+ */
 @Implements(DateFormat.class)
 public class ShadowDateFormat {
+
+    public static final char DATE = 'd';
+    public static final char MONTH = 'M';
+    public static final char YEAR = 'y';
 
 	@Implementation
 	public final static java.text.DateFormat getDateFormat(Context context) {
 		return java.text.DateFormat.getDateInstance();
 	}
+
+    @Implementation
+    public static char[] getDateFormatOrder(Context context) {
+        char[] order = new char[] { DATE, MONTH, YEAR };
+        String value = getDateFormatString(context);
+        int index = 0;
+        boolean foundDate = false;
+        boolean foundMonth = false;
+        boolean foundYear = false;
+
+        for (char c : value.toCharArray()) {
+            if (!foundDate && (c == DATE)) {
+                foundDate = true;
+                order[index] = DATE;
+                index++;
+            }
+
+            if (!foundMonth && (c == MONTH)) {
+                foundMonth = true;
+                order[index] = MONTH;
+                index++;
+            }
+
+            if (!foundYear && (c == YEAR)) {
+                foundYear = true;
+                order[index] = YEAR;
+                index++;
+            }
+        }
+        return order;
+    }
+
+    @Implementation
+    private static String getDateFormatString(Context context) {
+        return "%s/%s/%s";
+    }
+
 }
